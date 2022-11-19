3d0eeb08c0f2515471bbf65f2e3b6c2224a22083
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2022-11-19 08:52:35 +0100

    Initial Commit

---
+++ IOS Concurrency/IOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_ConcurrencyApp.swift
+//  IOS Concurrency
+//
+//  Created by Eric on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
